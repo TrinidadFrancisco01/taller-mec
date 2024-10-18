488ce21ced7fd3771d2eaa2c8bb9db3351235abb
TrinidadFrancisco01 <atrinifco2004@gmail.com>
2024-10-18 17:42:21 -0600

    creacion de nueva carpeta

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,18 +4,21 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilita CORS para permitir solicitudes desde http://localhost:4200
+  // Habilita CORS para permitir solicitudes desde ambos orígenes
   app.enableCors({
-    origin: 'http://tallerheber.losdela.com',  // Reemplaza con la URL de tu frontend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  // Métodos permitidos
+    origin: [
+      'https://tallerheber.losdela.com',  // Origen HTTPS
+      'http://tallerheber.losdela.com',   // Origen HTTP
+      'http://localhost:4200',          // Origen local para desarrollo
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',  // Métodos permitidos
     credentials: true,  // Si necesitas habilitar el envío de cookies o credenciales
   });
-  // Establece un puerto estático. En local será 3000, pero en Vercel se asignará dinámicamente.
+
   const port = 3000;
   await app.listen(port);
-
-  // Imprime la URL del servidor dinámicamente según dónde esté corriendo
   const serverUrl = await app.getUrl();
   console.log(`Servidor ejecutándose en ${serverUrl}`);
 }
+
 bootstrap();
